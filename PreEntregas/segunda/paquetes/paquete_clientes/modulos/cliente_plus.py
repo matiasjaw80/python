2e8629f42b80aceba52614e7ba9b745525babc11
matiasjaw80 <matiasjaw80@gmail.com>
2024-06-05 20:40:47 -0300

    first commit

---
+++ PreEntregas/segunda/paquetes/paquete_clientes/modulos/cliente_plus.py
@@ -0,0 +1,11 @@
+from paquetes.paquete_clientes.modulos.cliente import Cliente
+
+class ClientePlus(Cliente):
+    def __init__(self, nombre: str, apellido: str, correo_electronico: str, direccion: str, descuento: float):
+        super().__init__(nombre, apellido, correo_electronico, direccion)
+        self.descuento = descuento
+
+    def aplicar_descuento(self, precio: float) -> float:
+        descuento_aplicado = precio * (self.descuento / 100)
+        precio_final = precio - descuento_aplicado
+        return precio_final
---
+++ PreEntregas/segunda/paquetes/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="paquete_clientes",
+    version="1.0",
+    description="SegundaPre-Entrega",
+    author="Werjman Matias",
+    author_email="matiasjaw80@gmail.com",
+    packages=["paquete_clientes"],
+    include_package_data=True,
+)
