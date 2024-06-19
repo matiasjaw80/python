4ec05ea1b08dc6099ad007603df42b7cc27b594a
matiasjaw80 <matiasjaw80@gmail.com>
2024-06-19 20:16:32 -0300

    2do

--- PreEntregas/segunda/paquetes/setup.py
+++ PreEntregas/segunda/paquetes/setup.py
@@ -6,6 +6,6 @@ setup(
     description="SegundaPre-Entrega",
     author="Werjman Matias",
     author_email="matiasjaw80@gmail.com",
-    packages=["paquete_clientes"],
+    packages=["paquete_clientes"], 
     include_package_data=True,
 )
